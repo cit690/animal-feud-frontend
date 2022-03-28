bc613690398b98270509c7c4bec993cdd4dac7b0
Claire Thompson <clairethompson@Claires-MacBook-Pro.local>
2022-03-28 15:57:25 -0400

    idk

--- src/pages/PostThread/PostThread.jsx
+++ src/pages/PostThread/PostThread.jsx
@@ -1,19 +1,21 @@
+import { getAutoHeightDuration } from '@mui/material/styles/createTransitions'
 import {useState, useRef, useEffect} from 'react'
 import { useParams, useLocation } from 'react-router-dom'
 import { getThread } from '../../services/postService'
 
-function PostThread(props) {
+const PostThread = (props) => {
   const location = useLocation()
   const [postThread, setPostThread] = useState({})
   const { id } = useParams()
   console.log(id)
   useEffect(() => {
+    
+  },[])
 
-  },[id])
 return(<>
   <div>
     <h1>Post Thread</h1>
-    <h2> {postThread._id} </h2>
+    <h2> {id} </h2>
   </div>
 </>)
 }
